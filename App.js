71cffdb735d8f0cf76dbd4ac1333c15b6506f687
Joshua <amjoshua59@gmail.com>
2024-05-27 11:46:10 +0000

    Added readme

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={{ fontSize: 24 }}>
+      <Text style={{ fontSize: 24, color: "white" }}>
         My name is <Text style={{ fontWeight: "bold" }}>Joshua</Text>
       </Text>
       <StatusBar style="auto" />
---
+++ readme.md
@@ -0,0 +1,6 @@
+## 11024515
+
+![App screenshot](./assets/Screenshot_20240527-113627.jpg)
+
+This is an app that displays a text stating my name with my name being made bolder than the rest of the other text.
+
